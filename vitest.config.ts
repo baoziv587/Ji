eb6b56633a7aa65f3788849738ec9afa950c08e3
baoziv587 <7347513+baoziv587@users.noreply.github.com>
2026-09-25 01:46:33 -0700

    feat: add coverage script and configuration for Vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,9 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     include: ['{packages,apps}/*/src/**/*.test.ts'],
+    coverage: {
+      include: ['packages/*/src/**/*.ts'],
+      exclude: ['**/*.test.ts'],
+    },
   },
 })
